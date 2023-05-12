f3887af916038ed0bf2049d60f356b9d566fa4bc
adrianrodriguezp <aarodriguezp@ufpso.edu.co>
2023-05-13 01:39:11 +0200

    hoja de vida

--- js/myjs.js
+++ js/myjs.js
@@ -1,1 +1,22 @@
-alert("HOLA BIENVENIDO");
+let enlaces_header = document.querySelectorAll(".enlaces-header")[0];
+let icon_hamburger = true;
+
+document.querySelectorAll(".hamburger")[0].addEventListener("click", function(){
+if(icon_hamburger){
+    document.querySelectorAll(".hamburger")[0].style.color="#fff";
+    icon_hamburger=false;
+}else{
+    document.querySelectorAll(".hamburger")[0].style.color="#000";
+    icon_hamburger=true;
+}
+
+enlaces_header.classList.toggle("hamburgeropen")
+})
+
+document.querySelector(".enalces-header")[0].addEventListener("click" , function(){
+
+    enlaces_header.classList.toggle("hamburgeropen");
+    document.querySelectorAll(".hamburger")[0].style.color="#000";
+
+
+})
